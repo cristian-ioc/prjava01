38d6f90c0731b22af31f23668a264b4f58fc490d
cristian <cristian@192.168.1.147>
2021-05-13 23:03:05 +0200

    Versió 0.2 del projecte prjava01

--- src/prjava01/prjava01.java
+++ src/prjava01/prjava01.java
@@ -28,6 +28,8 @@ public class prjava01 {
             bw.newLine();
             bw.write("  <body>");
             bw.newLine();
+            bw.write(" <h1>DAW2 m08uf4eac4</h1>");
+            bw.newLine();
             bw.write("    Nova p&agrave;gina web");
             bw.newLine();
             bw.write("  </body>");
